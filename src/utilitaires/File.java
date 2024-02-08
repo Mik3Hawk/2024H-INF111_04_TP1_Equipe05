f5907ba2c8198b4b402ef5b6fe4322e9f4a821f7
Noah Tremblay <noah.tremblay.1@ens.etsmtl.ca>
2024-02-08 15:08:25 -0500

    mini push

--- src/utilitaires/File.java
+++ src/utilitaires/File.java
@@ -2,4 +2,13 @@ package utilitaires;
 
 public class File {
 
+    private Object[] donnees;
+    private int premier,dernier;
+
+    public int taille(){
+        int n ;
+        n= this.donnees.length;
+        return (n - premier + dernier) % n;
+    }
+
 }
