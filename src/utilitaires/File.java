38e429e45a63b9b00e8e520b14fffa0810f3d696
Noah Tremblay <noah.tremblay.1@ens.etsmtl.ca>
2024-02-11 23:08:17 -0500

    update Noah

--- src/utilitaires/File.java
+++ src/utilitaires/File.java
@@ -12,13 +12,48 @@ package utilitaires;
  * @author Noah Tremblay, ETS
  * @version Fev, 2024
  */
-public class Noeud{
+
+/**
+ * classe noeud pour représenter les objets ainsi que leur suivant.
+ */
+class Noeud{
     Object donnee;
-    File.Noeud suivant;
+    Noeud suivant;
+    public Noeud(Object data, Noeud next) {
+        this.donnee = data;
+        this.suivant = next;
+    }
 }
 public abstract class File {
     private Noeud premier,dernier;
-    pivate taille;
+    private int taille;
+
+    public File(Object premier){
+        Noeud n = new Noeud(premier,null) ;
+        this.premier = n;
+        this.dernier = n;
+        this.taille++;
+    }
+    public void ajouterElement(Object element){
+        Noeud n = new Noeud(element,null);
+        this.dernier.suivant = n;
+        this.taille++;
+    }
+    public Object enleverElement(){
+        if (this.taille <=0){
+            System.out.println("ERREUR impossible de diviser "+this+"par 0!");
+            System.exit(1); //code d'erreur
+        }
+        if (this.taille ==1){ //PAS FINI
+            System.out.println("ERREUR impossible de diviser "+this+"par 0!");
+            System.exit(1); //code d'erreur
+        }
+
+        Noeud temp = this.premier;
+        this.premier = temp.suivant;
+        this.taille--;
+        return temp.donnee;
+    }
 
 
 }
