4c65ef37f2dafa1fc02c5fcb370616d602bf347b
Noah Tremblay <noah.tremblay.1@ens.etsmtl.ca>
2024-02-16 14:24:26 -0500

    Update Noah

--- src/modele/centreControle/CentreControle.java
+++ src/modele/centreControle/CentreControle.java
@@ -1,4 +1,19 @@
 package modele.centreControle;
+import modele.communication.Message;
+import modele.communication.TransporteurMessage;
 
-public class CentreControle {
+public class CentreControle extends TransporteurMessage {
+
+
+
+    // Définitions des méthodes abstraites
+    @Override
+    protected void envoyerMessage(Message msg) {
+
+    }
+
+    @Override
+    protected void gestionnaireMessage(Message msg) {
+
+    }
 }
--- src/modele/rover/Rover.java
+++ src/modele/rover/Rover.java
@@ -1,4 +1,18 @@
 package modele.rover;
 
-public class Rover {
+import modele.communication.Message;
+import modele.communication.TransporteurMessage;
+public class Rover extends TransporteurMessage {
+
+
+    // Définitions des méthodes abstraites
+    @Override
+    protected void envoyerMessage(Message msg) {
+
+    }
+    @Override
+    protected void gestionnaireMessage(Message msg) {
+
+    }
+
 }
