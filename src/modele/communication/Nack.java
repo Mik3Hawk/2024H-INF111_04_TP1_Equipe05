ddd5c93a92c50ba2194f51e56bf5bfd7f3ecb174
Noah Tremblay <noah.tremblay.1@ens.etsmtl.ca>
2024-02-16 14:40:36 -0500

    Update Noah

--- src/modele/communication/Nack.java
+++ src/modele/communication/Nack.java
@@ -1,9 +1,13 @@
 package modele.communication;
 
 public class Nack extends Message{
+
+    /**
+     * constructeur qui fait appel au constructeur mère
+     * @param compte, identifiant unique et incrémental pour indiquer la position du message
+     */
     public Nack(int compte){
         super(compte);
-
     }
 
 }
--- src/modele/communication/NoOp.java
+++ src/modele/communication/NoOp.java
@@ -2,8 +2,11 @@ package modele.communication;
 
 public class NoOp extends Message{
 
+    /**
+     * constructeur qui fait appel au constructeur mère
+     * @param compte, identifiant unique et incrémental pour indiquer la position du message
+     */
     public NoOp(int compte){
         super(compte);
-
     }
 }
